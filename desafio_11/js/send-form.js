cbf1df5f35388cbf3138599e2c78ade39c9f8076
enzomartindev <enzomartindev@outlook.com>
2023-12-11 20:19:23 -0300

    avance 3

--- desafio_11/js/send-form.js
+++ desafio_11/js/send-form.js
@@ -24,5 +24,80 @@ function loadRequest() {
 
 }
 
-
 loadRequest();
+
+
+
+const inputUser = document.getElementById("username");
+const inputStreet = document.getElementById("street");
+const inputStNumber = document.getElementById("street-number");
+const btnConfirm = document.getElementById("btn-confirm");
+
+/*regex*/
+const regexName = /\b[a-zA-Z]{2,50}\b/; //Que contenga un string de al menos 2 letras
+const regexNumber = /^[0-9]+$/; //Sólo números
+
+/*Functions*/
+function validateUsername() {
+    if (!regexName.test(inputUser.value)) {
+        inputUser.nextElementSibling.innerText = "El usuario ingresado es inválido";
+        inputUser.classList.add("input-invalid");
+        return false;
+    } else {
+        inputUser.nextElementSibling.innerText = "";
+        inputUser.classList.remove("input-invalid");
+        inputUser.classList.add("input-valid");
+        return true;
+    }
+}
+
+function validateStreet() {
+    if (!regexName.test(inputStreet.value)) {
+        inputStreet.nextElementSibling.innerText = "La calle ingresada es inválida";
+        inputStreet.classList.add("input-invalid");
+        return false;
+    } else {
+        inputStreet.nextElementSibling.innerText = "";
+        inputStreet.classList.remove("input-invalid");
+        inputStreet.classList.add("input-valid");
+        return true;
+    }
+}
+
+function validateStNumber() {
+    if (!regexNumber.test(inputStNumber.value)) {
+        inputStNumber.nextElementSibling.innerText = "El valor ingresado es inválido";
+        inputStNumber.classList.add("input-invalid");
+        return false;
+
+    } else {
+        inputStNumber.nextElementSibling.innerText = "";
+        inputStNumber.classList.remove("input-invalid");
+        inputStNumber.classList.add("input-valid");
+        return true;
+    }
+}
+
+function validateRequest() {
+    if (validateUsername()
+        && validateStreet()
+        && validateStNumber()) {
+        //Muestra mensaje de éxito si todos los valores son  válidos
+        //showSuccess();
+        //buttonSave.disabled = true;
+
+    } else {
+        //Si hay algún valor invalido muestro alert
+        //showAlert();
+    }
+
+}
+
+function saveProduct() {
+    validateUsername();
+    validateStreet();
+    validateStNumber();
+
+}
+
+btnConfirm.onclick = saveProduct;
