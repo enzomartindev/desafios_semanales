9203ef605d99fe9b47c50187148080347c8ac861
enzomartindev <enzomartindev@outlook.com>
2024-02-21 23:07:14 -0300

    avance1

---
+++ desafio_16/src/App.jsx
@@ -0,0 +1,24 @@
+import Header from "./components/layout/Header";
+import Main from "./components/layout/Main";
+import Footer from "./components/layout/Footer";
+
+//import { useState } from "react";
+
+import "./app.scss";
+import ProductList from "./components/ProductList ";
+
+const App = () => {
+
+
+    return (
+        <>
+            <Header title="Desafío semanal 16" subtitle="Productos" />
+            <Main>
+                <ProductList/>
+            </Main>
+            <Footer />
+        </>
+    );
+};
+
+export default App;
---
+++ desafio_16/src/components/CreateProduct.jsx
@@ -0,0 +1,10 @@
+
+const CreateProduct = () => {
+    return (
+        <div className="productCard">
+            <p>Crear Producto!</p>
+        </div>
+    );
+};
+
+export default CreateProduct;
